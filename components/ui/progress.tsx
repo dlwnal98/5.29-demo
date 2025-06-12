1602a16aaa06fa23ee345563bee7bc19b436fdb9
dlwnal98 <pater0232@gmail.com>
2025-06-12 13:22:38 +0900

    vercel v0 v11에서 수정한 부분 커밋

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -7,8 +7,10 @@ import { cn } from "@/lib/utils"
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & {
+    status?: "UP" | "DOWN" | "OUT_OF_SERVICE" | "InstancesByZone"
+  }
+>(({ className, value, status, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
@@ -18,7 +20,9 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
+      className={`h-full w-full flex-1 transition-all ${
+                           status === "UP" ? "bg-green-500" : status === "DOWN" ? "bg-red-500" :  status === "OUT_OF_SERVICE" ? "bg-yellow-500" : "bg-purple-500"
+                          }`}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
