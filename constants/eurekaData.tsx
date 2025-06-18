64d524e60ee1de1a971e5ddd93082b56ecf6b4a6
dlwnal98 <pater0232@gmail.com>
2025-06-18 11:28:12 +0900

    .

--- constants/eurekaData.tsx
+++ constants/eurekaData.tsx
@@ -29,8 +29,9 @@ export const eurekaServicesData = [
     serviceName: "AUTH-SERVER",
     instanceCount: 2,
     statusSummary: {
-      UP: 1,
-      DOWN: 1,
+      up: 1,
+      down: 1,
+      outOfService: 1,
     },
     zones: ["kr-a", "kr-b"],
     versions: ["1.0.0", "1.0.1"],
@@ -116,7 +117,7 @@ export const eurekaServicesData = [
 export const instanceData = {
   instanceId: "user-service:8081",
   hostName: "localhost",
-  app: "USER-SERVICE",
+  appName: "USER-SERVICE",
   ipAddr: "172.18.0.6",
   status: "UP",
   overriddenStatus: "UNKNOWN",
@@ -125,14 +126,6 @@ export const instanceData = {
   securePort: 443,
   isSecurePortEnabled: false,
   dataCenter: "MyOwn",
-  leaseInfo: {
-    renewalIntervalInSecs: 30,
-    durationInSecs: 90,
-    registrationTimestamp: 1749581820000,
-    lastRenewalTimestamp: 1749582882000,
-    evictionTimestamp: 0,
-    serviceUpTimestamp: 1749581830000,
-  },
   metadata: {
     version: "1.5.2",
     gitCommit: "e6b3c2d",
