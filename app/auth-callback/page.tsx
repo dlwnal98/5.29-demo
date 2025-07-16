5c7d547a731ffcbfa27c50bf02fce628ecaac0f2
dlwnal98 <pater0232@gmail.com>
2025-07-16 17:39:39 +0900

    .

--- app/auth-callback/page.tsx
+++ app/auth-callback/page.tsx
@@ -1,5 +1,9 @@
 // app/auth-callback/page.tsx
-import AuthCallbackClient from './AuthCallbackClient';
+import dynamic from 'next/dynamic';
+
+const AuthCallbackClient = dynamic(() => import('./AuthCallbackClient'), {
+  ssr: false, // 🔥 클라이언트에서만 렌더링하도록 명시
+});
 
 export default function AuthCallbackPage() {
   return <AuthCallbackClient />;
